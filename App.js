df75f3cf241fde6f4dd271256ae453af12f4c37d
elgodolfredo <psilva@monssa.com.ar>
2021-01-26 09:09:18 -0300

    notifs con expo

--- App.js
+++ App.js
@@ -4,30 +4,92 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens'
 import {decode, encode} from 'base-64'
+import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
+
+
+// This refers to the function defined earlier in this guide, in Push Notifications Set Up
+registerForPushNotificationsAsync = async () => {
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    const token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+    // this.setState({ expoPushToken: token });
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+};
+
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+
 if (!global.btoa) {  global.btoa = encode }
 if (!global.atob) { global.atob = decode }
 
 const Stack = createStackNavigator();
 
-export default function App() {
+export default class App extends React.Component {
 
-  const [loading, setLoading] = useState(true)
-  const [user, setUser] = useState(null)
+  state = {
+    notification: {},
+    user : null,
+    loading : true,
+  };
 
-  return (
-      // { user ? (
-      // ) : (
-        // )}
-    <NavigationContainer>
-      <Stack.Navigator>
-          <>
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Registration" component={RegistrationScreen} />
-            <Stack.Screen name="Home">
-              {props => <HomeScreen {...props} extraData={user} />}
-            </Stack.Screen>
-          </>
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  componentDidMount() {
+    registerForPushNotificationsAsync();
+
+    Notifications.addNotificationReceivedListener(this._handleNotification);
+    
+    Notifications.addNotificationResponseReceivedListener(this._handleNotificationResponse);
+  }
+
+  _handleNotification = notification => {
+    this.setState({ notification: notification });
+  };
+
+  _handleNotificationResponse = response => {
+    console.log(response);
+  };
+
+  render (){
+      return (
+        <NavigationContainer>
+        <Stack.Navigator>
+            <>
+              <Stack.Screen name="Login" component={LoginScreen} />
+              <Stack.Screen name="Registration" component={RegistrationScreen} />
+              <Stack.Screen name="Home">
+                {props => <HomeScreen {...this.props} extraData={this.state.user} />}
+              </Stack.Screen>
+            </>
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
